c12493afb25bde4ad4642f959b27f5455afade17
unknown <S2030382@W7S85V1DP1.ad.austinisd.org>
2015-03-13 09:35:05 -0500

    Finished basic scanning library

--- ScanLib.hs
+++ ScanLib.hs
@@ -8,7 +8,7 @@ module ScanLib where
 
 
 
-data Phrase a = Phrase Int (Char -> Bool) (Action a) Int
+data Clause a = Clause Int (Char -> Bool) (Action a) Int
 
 
 type Status a = (String, [a])
@@ -21,6 +21,9 @@ type Reader a = String -> a
 append :: Action a
 append (buff, tokens) char = (buff ++ [char], tokens)
 
+appendEmit :: Reader a -> Action a
+appendEmit reader (buff, tokens) char = ([], tokens ++ [reader (buff ++ [char])])
+
 emit :: Reader a -> Action a
 emit reader (buff, tokens) char = ([], tokens ++ [reader buff])
 
@@ -28,6 +31,35 @@ ignore :: Action a
 ignore a char = a
 
 
+clauseDo :: Clause a -> Char -> (Int, Status a) -> (Int, Status a)
+clauseDo c@(Clause cond predicate action result) char (state, status)
+    | matchesClause (state, char) c = (result, action status char)
+    | otherwise = (state, status)
+
+matchesClause :: (Int, Char) -> Clause a -> Bool
+matchesClause (state, char) (Clause cond predicate _ _) = (state == cond) && predicate char
+
+clausesDo :: [Clause a] -> Char -> (Int, Status a) -> (Int, Status a)
+clausesDo [] _ thing = thing
+clausesDo (clause:clauses) char (state, status) | matchesClause (state, char) clause = clauseDo clause char (state, status)
+    | otherwise = clausesDo clauses char (state, status)
 
 
 
+
+clausesDoStr' :: [Clause a] -> String -> (Int, Status a) -> (Int, Status a)
+clausesDoStr' clauses "" thing = thing
+clausesDoStr' clauses (char:chars) thing = clausesDoStr' clauses chars (clausesDo clauses char thing)
+
+clausesDoStr :: [Clause a] -> String -> (Int, Status a)
+clausesDoStr clauses str = clausesDoStr' clauses str (0, ([], []))
+
+
+
+readStr :: Reader String
+readStr str = read str
+
+scanStrClauses = [
+    Clause 0 (== '"') append 1,
+    Clause 1 (/= '"') append 1,
+    Clause 1 (== '"') (appendEmit readStr) 0]
