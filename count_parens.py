34445af4bcec4be7deb3b56c5d279497c6690021
HankComer <rodent05@gmail.com>
2015-03-16 10:49:57 -0500

    fixed variable names in count_parens.py still having issue loading utils.hl from repl

--- count_parens.py
+++ count_parens.py
@@ -3,16 +3,16 @@ def count_parentheses(text):
     rcount = 0
     index = 0
     for letter in text:
-	if letter == "(":
-	    lcount += 1
-	elif letter == ")":
-	    rcount += 1
-	if rcount > lcount:
-	    print "found an extra ')' at {0}".format(str(index))
-	    return
-	index += 1
-    if lparens > rparens:
-        print "you're missing {0} ')'".format(lparens - rparens)
+        if letter == "(":
+            lcount += 1
+        elif letter == ")":
+            rcount += 1
+        if rcount > lcount:
+            print "found an extra ')' at {0}".format(str(index))
+            return
+        index += 1
+    if lcount > rcount:
+        print "you're missing {0} ')'".format(lcount - rcount)
     else:
         print "your parentheses are balanced"
         
